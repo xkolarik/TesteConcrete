f3cd734cac30f34acd53adae1bf04a84d87e7190
xkolarik <paulo_kolarik@hotmail.com>
2016-10-12 13:05:34 -0300

    Teste Concrete
    Teste Concrete Solutions

---
+++ Crud-Spring-Boot-master/src/main/java/br/com/teste/concrete/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package br.com.teste.concrete.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
